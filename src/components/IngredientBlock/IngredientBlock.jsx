8063d2af472d0399c0cde4e00b8e392dafb1aa1a
김용민 <dydals3440@gmail.com>
2023-08-19 20:41:38 +0900

    fix: 세부레시피에서 식재료 이미지가 안보이는 부분 수정

--- src/components/IngredientBlock/IngredientBlock.jsx
+++ src/components/IngredientBlock/IngredientBlock.jsx
@@ -4,6 +4,15 @@ import * as S from './IngredientBlock.styles';
 
 import { INGREDIENT_ICON, INGREDIENT_ID } from 'constants/ingredient';
 
+const findValidIcon = (ingreId) => {
+  for (let i = 0; i < INGREDIENT_ICON.length; i++) {
+    if (INGREDIENT_ICON[i][INGREDIENT_ID[ingreId]]) {
+      return INGREDIENT_ICON[i][INGREDIENT_ID[ingreId]];
+    }
+  }
+  return null;
+};
+
 const IngredientBlock = (props) => {
   const { mainTitle, ingredient } = props;
 
@@ -11,13 +20,16 @@ const IngredientBlock = (props) => {
     <S.Container>
       <p>{mainTitle}</p>
       <S.IngredientBox>
-        {ingredient.map((ingre, idx) => (
-          <S.Ingredient key={idx}>
-            {INGREDIENT_ICON[0][INGREDIENT_ID[ingre.ingre_id]]}
-            <h5>{ingre.ingre_name}</h5>
-            <h5>{ingre.amount}</h5>
-          </S.Ingredient>
-        ))}
+        {ingredient.map((ingre, idx) => {
+          const selectedIcon = findValidIcon(ingre.ingre_id);
+          return (
+            <S.Ingredient key={idx}>
+              {selectedIcon}
+              <h5>{ingre.ingre_name}</h5>
+              <h5>{ingre.amount}</h5>
+            </S.Ingredient>
+          );
+        })}
       </S.IngredientBox>
     </S.Container>
   );
